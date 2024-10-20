9bd1def454573d93311264e036c3b61d037c9784
Bùi Huy Tuyền <buihuytuyen9912@gmail.com>
2024-10-20 16:39:46 +0700

    add type cho i18n

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import en from './messages/en.json';
+
+type Messages = typeof en;
+
+declare global {
+  // Use type safe message keys with `next-intl`
+  interface IntlMessages extends Messages {}
+}
